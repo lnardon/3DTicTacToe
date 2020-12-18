6d08bdd5f7dd3cdfa6fd54fd614f47481a3345c8
Lnardon <lucaslnardon@gmail.com>
2020-12-18 04:25:28 -0300

    :sparkles: Add logic to check game winner

--- index.js
+++ index.js
@@ -61,10 +61,16 @@ function onClick() {
         } else {
           aux[0].material.color = new THREE.Color(0x00eee1);
         }
-        currentPlayer = !currentPlayer;
-        document.getElementsByClassName(
-          "player"
-        )[0].innerText = `Current Player: ${currentPlayer ? "Red" : "Blue"}`;
+        let result = checkForWinner();
+        if (result) {
+          alert(`The winner is: ${result ? "Red" : "Blue"}`);
+          createGame();
+        } else {
+          currentPlayer = !currentPlayer;
+          document.getElementsByClassName(
+            "player"
+          )[0].innerText = `Current Player: ${currentPlayer ? "Red" : "Blue"}`;
+        }
       }
     }
   }
@@ -80,29 +86,9 @@ light2.position.set(0, 7, 0);
 scene.add(light1);
 scene.add(light2);
 
-//OBJECT
-let squares = [];
-let x = 0;
-let z = -2;
-for (let i = 0; i < 9; i++) {
-  const geometry = new THREE.BoxGeometry(20, 5, 20);
-  const material = new THREE.MeshLambertMaterial({ color: 0xfafafa });
-  const square = new THREE.Mesh(geometry, material);
-  if (i % 3 === 0) {
-    x = -1;
-    z++;
-  }
-  square.rotateX(Math.PI / 1);
-  square.rotateY(Math.PI / 2);
-  square.position.set(23 * x, -10, 23 * z);
-  square.inUse = false;
-  squares.push(square);
-  scene.add(squares[i]);
-  x++;
-}
-
 //RENDER LOOP
 let currentPlayer = true;
+let squares = [];
 requestAnimationFrame(render);
 function render() {
   raycaster.setFromCamera(mouse, camera);
@@ -121,4 +107,62 @@ function render() {
   controls.update();
   renderer.render(scene, camera);
   requestAnimationFrame(render);
+}
+
+// Helpers
+function createGame() {
+  squares = [];
+  let x = 0;
+  let z = -2;
+  for (let i = 0; i < 9; i++) {
+    const geometry = new THREE.BoxGeometry(20, 5, 20);
+    const material = new THREE.MeshLambertMaterial({ color: 0xfafafa });
+    const square = new THREE.Mesh(geometry, material);
+    if (i % 3 === 0) {
+      x = -1;
+      z++;
+    }
+    square.rotateX(Math.PI / 1);
+    square.rotateY(Math.PI / 2);
+    square.position.set(23 * x, -10, 23 * z);
+    square.inUse = false;
+    squares.push(square);
+    scene.add(squares[i]);
+    x++;
+  }
+}
+createGame();
+
+function checkForWinner() {
+  let winner = null;
+
+  for (let i = 0; i < 3; i++) {
+    // X Axis
+    if (equals(squares[3 * i], squares[3 * i + 1], squares[3 * i + 2])) {
+      winner = squares[3 * i];
+    }
+
+    // Y Axis
+    if (equals(squares[i], squares[i + 3], squares[i + 6])) {
+      winner = squares[i];
+    }
+  }
+
+  // XY-Axis
+  if (equals(squares[0], squares[4], squares[8])) {
+    winner = squares[0];
+  }
+  if (equals(squares[2], squares[4], squares[6])) {
+    winner = squares[2];
+  }
+
+  if (winner !== null) {
+    return winner.player;
+  }
+}
+
+function equals(a, b, c) {
+  return (
+    a.player === b.player && b.player === c.player && a.player !== undefined
+  );
 }
